54a26de8be2f7fe0768b297ec88ff5d60011549f
Stephen Karuku <s.karuku.sk@gmail.com>
2026-01-05 17:59:13 +0300

    updates

--- components/includes/footer.js
+++ components/includes/footer.js
@@ -90,9 +90,6 @@ function Footer(){
                 <ul className="sponsors-list">
                   <li className="mb-0"><a target="_blank" href="https://www.authorandrewsmith.com">Xoilac xem bong da</a></li>
                   <li className="mb-0"><a target="_blank" href="https://www.docandrewmurray.com">Socolive truc tiep bong da</a></li>
-                  <li className="mb-0"><a target="_blank" href="https://ok365.shop">trang chủ OK365</a></li>
-                  <li className="mb-0"><a target="_blank" href="https://69vn.ph">trang chủ 69VN</a></li>
-                  <li className="mb-0"><a target="_blank" href="https://79king.reisen">trang chủ 79King</a></li>
                 </ul>
             </div>
         </div>
