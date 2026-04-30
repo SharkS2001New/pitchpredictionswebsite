6d3ad2be6525bb77dd819d7240ba07a0c215e633
Stephen Karuku <s.karuku.sk@gmail.com>
2026-04-30 15:20:40 +0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-export default nextConfig;
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if 
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
