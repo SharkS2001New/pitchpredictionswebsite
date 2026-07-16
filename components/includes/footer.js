af4bba6a93b3f4aa6b06b581f2095e1303bcca80
SharkS2001New <alpacke.tech@gmail.com>
2026-07-16 15:41:39 +0300

    Add new sponsor link 'bongdalu' to footer component with expiration details for enhanced partnership visibility.

--- components/includes/footer.js
+++ components/includes/footer.js
@@ -78,6 +78,10 @@ const sponsors = [
   /* New text link, first added 12-07-2026, expires 12-08-2026  New partner from tafatips.com*/
   { label: 'socolive',                     url: 'https://socolive4.com/' },
   /* expires 12-08-2026 end */
+
+  /* New text link, first added 16-07-2026, expires 16-08-2026  New partner from tafatips.com*/
+  { label: 'bongdalu',                     url: 'https://bongdalu17.net/' },
+  /* expires 16-08-2026 end */
 ];
 
 const LINK_COLOR = "#0d6efd"; // standard clickable link blue
